00195a888d0ab9d57c0ac7f09e7c97d376831629
Kirk Haines <wyhaines@gmail.com>
2016-09-08 06:00:21 +0900

    Do writes through the io engine, and not the connection directly.

--- lib/scrawls/core.rb
+++ lib/scrawls/core.rb
@@ -20,52 +20,52 @@ class SimpleRubyWebServer
     @io_engine.run @config
   end
 
-  def process request, connection
+  def process request, ioengine
     # This server is stupid. For any request method, and http version, it just tries to serve a static file.
     path = File.join( @config[:docroot], request['PATH_INFO'] )
     if FileTest.directory? path
-      deliver_directory path, connection
+      deliver_directory path, ioengine
     elsif FileTest.exist?( path ) and FileTest.file?( path ) and File.expand_path( path ).index( File.expand_path( @config[:docroot] ) ) == 0
-      connection.write CANNED_OK +
+      ioengine.send_data CANNED_OK +
           "Content-Type: #{MIME::Types.type_for( path )}\r\n" +
           "Content-Length: #{File.size( path )}\r\n" +
           "Last-Modified: #{File.mtime( path )}\r\n" +
           final_headers +
           File.read( path )
     else
-      deliver_404 connection
+      deliver_404 ioengine
     end
   rescue Exception => e
     puts "ERROR\n\n#{e}\n#{e.backtrace.join("\n")}\n"
-    deliver_500 connection
+    deliver_500 ioengine
   end
 
   def final_headers
     "Date: #{Time.now.httpdate}\r\nConnection: close\r\n\r\n"
   end
 
-  def deliver_directory path, connection
-    deliver_403 connection
+  def deliver_directory path, ioengine
+    deliver_403 ioengine
   end
 
-  def deliver_404 uri, connection
+  def deliver_404 uri, ioengine
     buffer = "The requested resource (#{uri}) could not be found."
-    connection.write "HTTP/1.1 404 Not Found\r\nContent-Length:#{buffer.length}\r\nContent-Type:text/plain\r\n#{final_headers}#{buffer}"
+    ioengine.send_data "HTTP/1.1 404 Not Found\r\nContent-Length:#{buffer.length}\r\nContent-Type:text/plain\r\n#{final_headers}#{buffer}"
   end
 
-  def deliver_400 connection
+  def deliver_400 ioengine
     buffer = "The request was malformed and could not be completed."
-    connection.write "HTTP/1.1 400 Bad Request\r\nContent-Length:#{buffer.length}\r\nContent-Type:text/plain\r\n#{final_headers}#{buffer}"
+    ioengine.send_data "HTTP/1.1 400 Bad Request\r\nContent-Length:#{buffer.length}\r\nContent-Type:text/plain\r\n#{final_headers}#{buffer}"
   end
 
-  def deliver_403 connection
+  def deliver_403 ioengine
     buffer = "Forbidden. The requested resource can not be accessed."
-    connection.write "HTTP/1.1 403 Bad Request\r\nContent-Length:#{buffer.length}\r\nContent-Type:text/plain\r\n#{final_headers}#{buffer}"
+    ioengine.send_data "HTTP/1.1 403 Bad Request\r\nContent-Length:#{buffer.length}\r\nContent-Type:text/plain\r\n#{final_headers}#{buffer}"
   end
 
-  def deliver_500 connection
+  def deliver_500 ioengine
     buffer = "There was an internal server error."
-    connection.write "HTTP/1.1 500 Bad Request\r\nContent-Length:#{buffer.length}\r\nContent-Type:text/plain\r\n#{final_headers}#{buffer}"
+    ioengine.send_data "HTTP/1.1 500 Bad Request\r\nContent-Length:#{buffer.length}\r\nContent-Type:text/plain\r\n#{final_headers}#{buffer}"
   end
 
   def content_type_for path
